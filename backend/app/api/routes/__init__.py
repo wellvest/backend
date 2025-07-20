fde27f59ed061017ac8304ba5dca03bcbc3c5f44
wellvest <wellvestltd@gmail.com>
2025-07-20 15:54:02 +0000

    fix

--- backend/app/api/routes/__init__.py
+++
@@ -1,2 +0,0 @@
-# Import all routes to make them available for import from app.api.routes
-from app.api.routes import auth, users, profile, plans, investments, wallets, network, referrals, uploads, noc, contact, auth_reset_password, bonus, notifications, otp, payments
